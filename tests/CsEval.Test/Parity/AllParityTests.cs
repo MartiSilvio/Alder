68981ddeb3e420c753eca384496a4559372671c4
Silvio Martignetti <silviomartignetti@gmail.com>
2026-02-10 21:51:21 +0300

    refactor(test): restructure TestData into ValidExpressions and InvalidExpressions

--- tests/CsEval.Test/Parity/AllParityTests.cs
+++ tests/CsEval.Test/Parity/AllParityTests.cs
@@ -8,19 +8,18 @@ namespace CsEval.Test.Parity;
 [TestFixture(CompilationMode.StrictCompiled)]
 public class AllParityTests(CompilationMode mode) : CsxParityTestsBase(mode)
 {
-    [TestCaseSource(nameof(DiscoverTestsRecursive), ["TestData"])]
+    [TestCaseSource(nameof(DiscoverTestsRecursive), ["TestData/ValidExpressions"])]
     public Task ParityTest(string csxPath) => RunMatchesCSharp(csxPath);
 }
 
 /// <summary>
 /// Universal parity test runner for invalid .csx files that should throw exceptions.
-/// Discovers all .csx files in NotAllowed folders recursively.
 /// </summary>
 [TestFixture(CompilationMode.Interpreted)]
 [TestFixture(CompilationMode.Compiled)]
 [TestFixture(CompilationMode.StrictCompiled)]
 public class AllParityThrowsTests(CompilationMode mode) : CsxParityTestsBase(mode)
 {
-    [TestCaseSource(nameof(DiscoverNotAllowedTestsRecursive), ["TestData"])]
+    [TestCaseSource(nameof(DiscoverTestsRecursive), ["TestData/InvalidExpressions"])]
     public Task ParityThrowsTest(string csxPath) => RunShouldThrow(csxPath);
 }
